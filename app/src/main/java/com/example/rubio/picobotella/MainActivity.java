0b13545288fb6f76b0607814a6b72c0f582f39e4
Rubio <rubian.llanos.7@gmail.com>
2018-04-18 16:16:43 -0500

    Ajustes en la interface grafica.

--- app/src/main/java/com/example/rubio/picobotella/MainActivity.java
+++ app/src/main/java/com/example/rubio/picobotella/MainActivity.java
@@ -20,7 +20,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getActionBar().hide();
         rand = new Random();
         botella = (ImageView)findViewById(R.id.botella);
         lanzar = (Button)findViewById(R.id.lanzar);
